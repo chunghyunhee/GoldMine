911f609872a06cce8d1b41d7882db9ecd241d2ae
user <chunghynuhee@gmail.com>
2020-08-13 16:12:34 +0900

    simulated annealing commit

--- experiment/hyper-params-search/hps/algorithms/ga/SimulatedAnnealing_3.py
+++ experiment/hyper-params-search/hps/algorithms/ga/SimulatedAnnealing_3.py
@@ -15,57 +15,53 @@ class SimulatedAnnealing(HPOptimizationAbstract):
         self._n_pop = 1
         self._k = 0.1
         self._M = self._hpo_params["M"]
-        self._N = self.hpo_params["N"]
-        self._beta = self._hpo_params["beta"]
         self._T0 = self._hpo_params["T0"]
         self._alpha = self._hpo_params["alpha"]
 
+    # neighbor selection
     def _generate(self, param_list, score_list):
-        # 초깃값
+        # 기준점 : x0
         result_param_list = list()
         x0 = hprs_info["pbounds"]["dropout_prob"]
 
         for i in range(self._M):
-            for j in range(self._N):
-                xt = 0
-                result_param_list = list()
-                # move operator
-                ran_x_1 = np.random.rand()
-                ran_x_2 = np.random.rand()
+            xt = 0
+            result_param_list = list()
 
-                if ran_x_1 >= 0.5:
-                    x1 = self._k*ran_x_2
-                else:
-                    x1 = -self._k*ran_x_2
+            # to make candidate : xt
+            ran_x_1 = np.random.rand()
+            ran_x_2 = np.random.rand()
 
-                xt = x0 + x1 # make candidate
+            if ran_x_1 >= 0.5:
+                x1 = self._k*ran_x_2
+            else:
+                x1 = -self._k*ran_x_2
 
-                # dict를 만들어서 전체 list에 넣는 형태
-                num_result_params = len(result_param_list)
-                result_param_list += self._generate_param_dict_list(num_result_params)
-                return result_param_list
+            xt = x0 + x1
 
+            num_result_params = len(result_param_list)
+            result_param_list += self._generate_param_dict_list(num_result_params)
+            return result_param_list
 
-    def neighbor(self, param_dict_list):
+    def accept(self, param_dict_list, result_param_list):
         temp = []
-        # 기준값과 새로 뽑은 값 중에서 개선된 값을 가져온다.
         best_params_list = list()
-        output_params_list = list()
-        output_params_list += self.optimize()
 
-        # 이웃점 vs 기준점 비교시에 개선되었는지를 확인
+        # 원래 score과 개선된 score을 확인하는 방법
+        of_new =
+        of_final =
+
         # 같으면 form을 확인하여 선택지 결정, 다르면 이웃을 선택한다.
-        if 개선 :
-            # neighbor값 선택
-            best_params_list += output_params_list
+        if param_dict_list == result_param_list :
+            best_params_list += param_dict_list
         else :
             ran_1 = np.random.rand()
             form = 1 / (np.exp((of_new - of_final) / self._T0))
             if ran_1 <= form:
-                # neighbor값 선택
-                best_params_list += output_params_list
+                best_params_list += result_param_list
             else :
-                # 기존의 것 사용
+                best_params_list += param_dict_list
+
         temp = np.append(temp, self._T0)
         self._T0 = self._alpha * self._T0
 
@@ -80,7 +76,7 @@ if __name__ == '__main__':
                 "update_n" : 5,
                 "n_steps": 10,
                 "n_params": 10,
-                "k_val": 5,
+                "k_val": 1,
                 "eval_key": "accuracy"
             },
         "ml_params":{

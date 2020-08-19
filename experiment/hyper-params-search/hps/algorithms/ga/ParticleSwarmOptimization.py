89ce7ea8998a71d3af9b65c27f912cac5f04aeb0
user <chunghynuhee@gmail.com>
2020-08-19 13:24:14 +0900

    modify parameter

--- experiment/hyper-params-search/hps/algorithms/ga/ParticleSwarmOptimization.py
+++ experiment/hyper-params-search/hps/algorithms/ga/ParticleSwarmOptimization.py
@@ -13,7 +13,6 @@ class ParticleSwarmOptimization:
     def _check_hpo_params(self):
         self._n_pop = self._n_params
         self._n_steps = self._hpo_params["n_steps"]
-        self._position_list = self._hpo_params["position_list"]
         self._bounds = self._hpo_params(["bounds"])
         self._w = self._hpo_params(["w"])       # constant of the inertia weight
         self._c1 = self._hpo_params(["c1"])     # cognitive constants
@@ -23,10 +22,11 @@ class ParticleSwarmOptimization:
     # generate candidate function
     def _generate(self, param_list, score_list):
         result_param_list = list()
+
         # generate random hyperparameter
         best_param_list = self._particle(param_list)
-        compute_velocity_params = self.compute_velocity(param_list)
-        update_position_params = self.update_position(param_list)
+        compute_velocity_params = self.compute_velocity(param_list, best_param_list)
+        update_position_params = self.update_position(param_list, compute_velocity_params)
 
         result_param_list += compute_velocity_params + update_position_params
         result_param_list = self._remove_duplicate_params(result_param_list)
@@ -49,34 +49,34 @@ class ParticleSwarmOptimization:
             return param_list
 
     # compute velocity to update position of particle
-    def compute_velocity(self, param_list, pos_best_i, position_i):
+    def compute_velocity(self, param_list, pos_best_i):
         # compute velocity of each particle's hyperparameter parameters
         velocity_i = list()
         for i in range(0, len(param_list)):
             r1 = random()
             r2 = random()
 
-            vel_cognitive = self._c1*r1*(pos_best_i[i] - position_i[i])
-            vel_social = self._c2*r2*(param_list[i] - position_i[i])
+            vel_cognitive = self._c1*r1*(pos_best_i[i] - param_list[i])
+            vel_social = self._c2*r2*(param_list[i] - param_list[i])
 
             velocity_i[i] = self._w * velocity_i[i] + vel_cognitive + vel_social
 
         return velocity_i
 
     # update the particle position based off new velocity updates
-    def update_position(self, param_list, position_i, velocity_i):
+    def update_position(self, param_list, velocity_i):
         for i in range(0, len(param_list)):
-            position_i[i] = position_i[i] + velocity_i[i]
+            param_list[i] = param_list[i] + velocity_i[i]
 
             # adjust max position if necessary
-            if position_i[i] > self._bounds[i][1]:
-                position_i[i] = self._bounds[i][1]
+            if param_list[i] > self._bounds[i][1]:
+                param_list[i] = self._bounds[i][1]
 
             # adjust min position if necessary
-            if position_i[i] < self._bounds[i][0]:
-                position_i[i] = self._bounds[i][0]
+            if param_list[i] < self._bounds[i][0]:
+                param_list[i] = self._bounds[i][0]
 
-        return position_i
+        return param_list
 
 
 # main __init__ to execute in this single file
@@ -84,11 +84,10 @@ if __name__ == '__main__':
     hprs_info = {
         "hpo_params" : {
                 "n_steps" : 100,
-                "position_list" : [1,2,3,4],
                 "bounds" : [0,10],
                 "w" : 0.1,
-                "c1": 10,
-                "c2": 1,
+                "c1": 3,
+                "c2": 3,
                 "eval_key": "accuracy"
             },
         "ml_params":{
